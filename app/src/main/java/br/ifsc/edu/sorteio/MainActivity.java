7ad0de00e4defac8b43fa73d5b5addf2b050312b
aluno <adsroot.aluno.ifsc.edu.br>
2020-02-27 11:27:20 -0300

    ok.

--- app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
@@ -1,29 +1,37 @@
 package br.ifsc.edu.sorteio;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     EditText editTextInicial, editTextFinal;
      TextView TextViewResultado;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextInicial = findViewById(R.id.editTextInicial);
-        editTextFinal = findViewById(R.id.editTextFinal);
-        View textViewResultado = findViewById(R.id.TextViewResultado);
     }
 
-    public void Sorteio(View view) {
-        int inicial = Integer.parseInt (editTextInicial.getText().toString());
+    public void SorteiaNumero (View view) {
 
-        textViewResultado.setText(Integer.toString(inicial));
+       TextView textView = findViewById(R.id.TextViewResultado);
+       EditText editTextInicial = findViewById(R.id.editTextInicial);
+        EditText editTextFinal = findViewById(R.id.EditTextfinal);
+
+        int valorInicial = Integer.parseInt(editTextInicial.getText().toString());
+        int valorFinal = Integer.parseInt(editTextFinal.getText().toString());
+
+       int  numeroSorteado = valorInicial + (int) (Math.random()*(valorFinal - valorInicial));;
+
+        textView.setText(Integer.toString(numeroSorteado));
+
+
     }
 }
